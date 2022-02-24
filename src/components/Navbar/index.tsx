0bcf7801c89479f177e07c692414c6bc6810a81b
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2022-02-24 18:15:35 -0300

    Updated i18n

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { availableLanguages } from '../../i18n';
+import '../../i18n';
 import { Nav, NavLink, NavMenu } from './styles';
 
 export function Navbar() {
   const { t, i18n } = useTranslation('navBar', { useSuspense: false });
+  const languages = t('languages');
 
   function defaultLanguage() {
     if (
@@ -33,13 +34,9 @@ export function Navbar() {
         defaultValue={defaultLanguage()}
         onChange={e => i18n.changeLanguage(e.target.value)}
       >
-        {availableLanguages.map(language => (
-          <option key={language} value={language}>
-            {language === 'pt_br'
-              ? 'Portuguese 🇧🇷'
-              : language === 'de'
-              ? 'German 🇩🇪'
-              : 'English 🇺🇸'}
+        {languages.map(language => (
+          <option key={language.id} value={language.value}>
+            {language.name}
           </option>
         ))}
       </select>
